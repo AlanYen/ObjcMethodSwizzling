6dd37e7fa39da6facd5f87cfe0ce3e6914e225b1
AlanYen <alanyen.mobile@gmail.com>
2015-09-04 15:47:01 +0800

    Initial Commit

---
+++ ObjcMethodSwizzling/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjcMethodSwizzling
+//
+//  Created by AlanYen on 2015/9/4.
+//  Copyright (c) 2015年 17Life. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
